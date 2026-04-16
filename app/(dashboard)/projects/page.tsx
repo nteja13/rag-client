a9ceab3ea9fc121cce57345ff958aedb6807f676
nteja13 <your.nteja13@gmail.com>
2026-04-16 12:10:49 +0530

    Add full client project

---
+++ app/(dashboard)/projects/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProjectsPage() {
+  return (
+    <div>This page shows the list of all the projects assosiated with the logged in user</div>
+  )
+}
+
+export default ProjectsPage
